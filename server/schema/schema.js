ead7d05f2f1b505712fc732b798b42388ecce0d4
Christopher Ljungblad <christopher@ljungbladsolutions.se>
2023-01-04 17:01:43 +0100

    Adding UserType and ScoreType and one-to-many relationship between User and Score

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -9,6 +9,7 @@ const {
   GraphQLObjectType,
   GraphQLID,
   GraphQLString,
+  GraphQLInt,
 } = graphql;
 
 var gameData = [
@@ -17,12 +18,56 @@ var gameData = [
   { id: "3", name: "Pokemon" },
 ];
 
-const GameType = new graphql.GraphQLObjectType({
+var userData = [
+  { id: "1", name: "Christopher" },
+  { id: "2", name: "Jakob" },
+  { id: "3", name: "Jane" },
+];
+
+var scoreData = [
+  { id: "1", score: 19, gameId: "1", userId: "1" },
+  { id: "2", score: 20, gameId: "1", userId: "2" },
+  { id: "3", score: 22, gameId: "1", userId: "3" },
+  { id: "4", score: 23, gameId: "2", userId: "1" },
+  { id: "5", score: 15, gameId: "2", userId: "2" },
+  { id: "6", score: 21, gameId: "2", userId: "3" },
+  { id: "7", score: 10, gameId: "3", userId: "1" },
+  { id: "8", score: 10, gameId: "3", userId: "2" },
+  { id: "9", score: 10, gameId: "3", userId: "1" },
+];
+
+const GameType = new GraphQLObjectType({
   name: "Game",
   description: "Game type",
   fields: {
     id: { type: GraphQLID },
     name: { type: GraphQLString },
+  },
+});
+
+const ScoreType = new GraphQLObjectType({
+  name: "ScoreType",
+  description: "User score for all games",
+  fields: {
+    id: { type: GraphQLID },
+    score: { type: GraphQLInt },
+    gameId: { type: GraphQLID },
+    userId: { type: GraphQLID },
+  },
+});
+
+const UserType = new GraphQLObjectType({
+  name: "User",
+  description: "User type",
+  fields: {
+    id: { type: GraphQLID },
+    name: { type: GraphQLString },
+    scores: {
+      type: GraphQLList(ScoreType),
+      resolve(parent, args) {
+        return _.filter(scoreData, { userId: parent.id });
+      },
+    },
   },
 });
 
@@ -46,6 +91,31 @@ const RootQuery = new GraphQLObjectType({
         return gameData;
       },
     },
+    user: {
+      type: UserType,
+      args: {
+        id: { type: GraphQLID },
+      },
+      resolve(parent, args) {
+        return _.find(userData, { id: args.id });
+      },
+    },
+
+    score: {
+      type: ScoreType,
+      args: {
+        id: { type: GraphQLID },
+      },
+      resolve(parent, args) {
+        return _.find(scoreData, { id: args.id });
+      },
+    },
+    // userScores: {
+    //   type: new GraphQLList(UserScoreType),
+    //   resolve(parent, args) {
+    //     return userScoreData;
+    //   },
+    // },
   },
 });
 
